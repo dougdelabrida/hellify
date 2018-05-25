82fedd56b0ace5710ad352f444d399bafc484558
Douglas Delabrida <dougdelabrida@gmail.com>
2018-05-25 12:31:41 -0300

    implemented getStyleElement

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,21 @@
+import { Block } from "./types";
+
 export function generateClassName () : string {
-    return `hell_${Math.random().toString(36).substring(2, 8)}`;
+  return `hell_${Math.random().toString(36).substring(2, 8)}`;
+}
+
+export function getStyleElement (block: Block) : HTMLElement {
+  const style = document.createElement('style');
+
+  (<any>Object).assign(style, {
+    id: `id_${block.className}`,
+    type: "text/css",
+    innerHTML: `
+      .${block.className} {
+        ${block.style.cssText}
+      }
+    `
+  });
+
+  return style;
 }
