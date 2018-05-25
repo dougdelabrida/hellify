817461481852f376f1b24c30db3eaf2ceba4dccf
Douglas Delabrida <dougdelabrida@gmail.com>
2018-05-25 12:27:54 -0300

    implemented mutation observer and registerBlock

--- src/engine.ts
+++ src/engine.ts
@@ -1,10 +1,11 @@
 import { Block } from "./types";
+import { getStyleElement } from "./utils";
 
 export class Engine {
   private _observer: MutationObserver;
   private _config: MutationObserverInit;
   private _target: HTMLElement;
-  private _blockList: any;
+  private _blockList: any = [];
 
   constructor(
     target: HTMLElement = document.body,
@@ -18,23 +19,33 @@ export class Engine {
 
     this._target = target;
     this._config = config;
-    this._blockList = [];
 
     this._observer.observe(this._target, this._config);
   }
 
-  private _mutationHandler (mutations: MutationRecord[]) : void {
-    console.log(mutations);
-    console.log(this._blockList);
+  private _mutationHandler = (mutations: MutationRecord[]) => {
+    this._blockList.forEach((block: Block) => {
+      let xpathResult: XPathResult = document.evaluate(block.xPath, this._target, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+  
+      for (let i = 0; i < xpathResult.snapshotLength; i++) {
+        let element: Element = (<Element>xpathResult.snapshotItem(i));
+        element.classList.add(block.className);
+      }
+    });
   }
 
   public registerBlock (block: Block) : void {
     const foundBlock: Block = this._blockList.find((_block: Block) => _block.xPath === block.xPath);
-
+    
     if (foundBlock) {
+      // TODO: Implements
       // Merge its style in case of duplication
       (<any>Object).assign(foundBlock.style, block.style);
     } else {
+      const style = getStyleElement(block);
+
+      document.head.insertBefore(style, document.head.lastChild);
+
       this._blockList.push(block);
     }
   }
